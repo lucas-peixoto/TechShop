c5471e5a9fd324d9c62395b4dd22cbce19cf1788
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2024-02-15 11:53:38 -0300

    Init gateway service

---
+++ gateway-service/src/main/java/br/com/gatewayservice/filter/AuthFilter.java
@@ -0,0 +1,16 @@
+package br.com.gatewayservice.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter implements GatewayFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+}
