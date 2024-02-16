d75b4e73a47b542b6e188f051e2868db5b0e29fc
Sergio Bezerra da Silva <50722925+sergiobsilva2505@users.noreply.github.com>
2024-02-16 18:41:04 -0300

    [Shop] init project

---
+++ product-shop/src/test/java/br/com/productshop/ProductShopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.productshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
