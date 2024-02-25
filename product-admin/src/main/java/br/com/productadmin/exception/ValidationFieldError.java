d148d1b7ad0064ab82d3c2ba070cdbfda2ef1230
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2024-02-25 11:01:30 -0300

    [Admin] Fix exception serialization error

--- product-admin/src/main/java/br/com/productadmin/exception/ValidationFieldError.java
+++ product-admin/src/main/java/br/com/productadmin/exception/ValidationFieldError.java
@@ -1,20 +1,4 @@
 package br.com.productadmin.exception;
 
-public class ValidationFieldError {
-
-    private final String field;
-    private final String message;
-
-    public ValidationFieldError(String field, String message) {
-        this.field = field;
-        this.message = message;
-    }
-
-    public String field() {
-        return field;
-    }
-
-    public String message() {
-        return message;
-    }
+public record ValidationFieldError(String field, String message) {
 }
