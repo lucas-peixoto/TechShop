d17a4de533f620776372b30ee1d0a02551f0c554
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2024-02-25 12:59:27 -0300

    [Shop] Configuring OpenFeign

---
+++ product-shop/src/main/java/br/com/productshop/user/LoggedUser.java
@@ -0,0 +1,21 @@
+package br.com.productshop.user;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class LoggedUser {
+
+    public Optional<User> getLoggedUser() {
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .filter(authentication -> authentication.getPrincipal() instanceof User)
+                .map(authentication -> (User) authentication.getPrincipal());
+    }
+
+    public boolean isLogged() {
+        return getLoggedUser().isPresent();
+    }
+}
