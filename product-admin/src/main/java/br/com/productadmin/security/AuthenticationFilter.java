1d68e3b1fe3dd4fc0016f4ea6016cff4c8d635fc
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2024-02-25 11:50:00 -0300

    [Admin] Configuring security

---
+++ product-admin/src/main/java/br/com/productadmin/security/AuthenticationFilter.java
@@ -0,0 +1,46 @@
+package br.com.productadmin.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.List;
+
+public class AuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (isMissingAuthorizationHeader(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String email = request.getHeader("email");
+        String role = request.getHeader("role");
+
+        tryToAuthenticate(email, role);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isMissingAuthorizationHeader(HttpServletRequest request) {
+        return request.getHeader("email") == null
+                || request.getHeader("email").isBlank()
+                || request.getHeader("role") == null
+                || request.getHeader("role").isBlank();
+    }
+
+    private void tryToAuthenticate(String email, String role) {
+        User userDetails = new User(email, "", true, true, true, true, AuthorityUtils.createAuthorityList(role));
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()));
+    }
+}
