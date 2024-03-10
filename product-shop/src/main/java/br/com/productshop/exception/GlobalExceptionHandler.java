5ae68eff9dff56faceccd19faf867def556b514f
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2024-03-10 15:42:00 -0300

    [Shop] Logging GlobalExceptionHandler errors

--- product-shop/src/main/java/br/com/productshop/exception/GlobalExceptionHandler.java
+++ product-shop/src/main/java/br/com/productshop/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package br.com.productshop.exception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,19 +13,24 @@ import java.util.List;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    public static Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         List<ValidationFieldError> fieldErrorList = e.getFieldErrors().stream().map(fieldError -> new ValidationFieldError(fieldError.getField(), fieldError.getDefaultMessage())).toList();
+        logger.error("Validation errors: {}", fieldErrorList);
         return ResponseEntity.badRequest().body(new ExceptionResponse.ValidationFieldErrors(fieldErrorList));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException() {
+        logger.error("Invalid JSON body");
         return ResponseEntity.badRequest().body(new ExceptionResponse.ExceptionMessage("Invalid JSON body"));
     }
 
     @ExceptionHandler(ValidationFieldsException.class)
     public ResponseEntity<ExceptionResponse> handleValidationFieldsException(ValidationFieldsException e) {
+        logger.error(e.getMessage(), e);
         return ResponseEntity.badRequest().body(new ExceptionResponse.ValidationFieldErrors(e.getFieldErrors()));
     }
 
@@ -34,6 +41,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleException(Exception e) {
+        logger.error(e.getMessage(), e);
         return ResponseEntity.internalServerError().body(new ExceptionResponse.ExceptionMessage(e.getMessage()));
     }
 }
